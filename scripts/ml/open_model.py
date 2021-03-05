cbb4a8d67a304d1c857df1874f104d18ffebd85b
Tomas Sladecek <tomas.sladecek@geneton.sk>
2021-03-05 09:53:54 +0100

    generalize openning models from gzipped files

--- scripts/ml/open_model.py
+++ scripts/ml/open_model.py
@@ -6,17 +6,16 @@ Open and return gzipped random forest model. Others will be left as jsons
 # %%
 import gzip
 import json
-from sklearn_json import from_json
-from sklearn_json.classification import deserialize_random_forest
+from sklearn_json import from_json, from_dict
 import xgboost as xgb
 
 
 def open_model(model_path):
-    if "randomforest" in model_path:
+    if model_path.endswith('gz'):
         with gzip.open(model_path, 'r') as f:
             model = f.read()
             model = json.loads(model.decode('utf-8'))
-            model = deserialize_random_forest(model)
+            model = from_dict(model)
         return model
     
     elif "xgboost" in model_path:
