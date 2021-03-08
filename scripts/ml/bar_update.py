c570a08c0ca48fa4ebc3cf77ab9803932097c823
Tomas Sladecek <tomas.sladecek@geneton.sk>
2021-03-08 12:50:42 +0100

    predict function + bar plots helper

---
+++ scripts/ml/bar_update.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Bar results update
+"""
+from scripts.ml.predict import predict
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# %%
+# results = {'label': [], 'correct': [], 'uncertain': [], 'incorrect': []}
+# model_path = 'results/models/xgboost_gain.json'
+# data_path = 'data/validation_gain.tsv.gz'
+# threshold = 0.95
+# custom_label = None
+
+def bar_update_results(results, model_path, data_path, threshold, custom_label=None):
+
+    model = model_path.split('/')[-1].split('_')[0]
+    cnv_type = model_path.split('/')[-1].split('_')[1]
+    
+    
+    if threshold == 0.5:
+        yhat, y = predict(model_path, data_path)
+        
+        results['uncertain'].append(0)
+    
+    else:
+        yhat, y = predict(model_path, data_path, proba=True)
+        t = (yhat >= threshold) + (yhat <= (1 - threshold))
+        yhat, y = yhat[t], y[t]
+        yhat = (yhat > 0.5) * 1
+        
+        results['uncertain'].append(np.sum(t == False))
+    
+    accuracy = np.mean(y == yhat)
+    
+    if custom_label:
+        results['label'].append(custom_label)
+    else:   
+        results['label'].append('{}\nAccuracy: {:.3f}'.format(model, accuracy))
+    
+    results['correct'].append(np.sum(y == yhat))
+    results['incorrect'].append(np.sum(y != yhat))
+    
+    return results
+    
+# %%
+# fig, ax = plt.subplots(1, 1, figsize = (12, 7))
+
+# res = pd.DataFrame(results)
+# res.iloc[::-1].set_index('label').plot(kind='barh', stacked=True, ax=ax, width=0.8,
+#                                        color=["#009900", "#C0C0C0", "#FF0000"])
+
+
---
+++ scripts/ml/predict.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+- Open and return gzipped random forest model. Others will be left as jsons
+- Predict binary target
+- Predict probabilities
+"""
+# %%
+import gzip
+import json
+from sklearn_json import from_json, from_dict
+import xgboost as xgb
+from scripts.constants import LOSS_ATTRIBUTES, GAIN_ATTRIBUTES
+from scripts.ml.prepare_df_for_training import prepare_df, prepare_test
+
+# %%
+def open_model(model_path):
+    if model_path.endswith('gz'):
+        with gzip.open(model_path, 'r') as f:
+            model = f.read()
+            model = json.loads(model.decode('utf-8'))
+            model = from_dict(model)
+        return model
+    
+    elif "xgboost" in model_path:
+        model = xgb.Booster()
+        model.load_model(model_path)
+        return model
+    
+    else:
+        model = from_json(model_path)
+        return model
+
+
+# %%
+def predict(model_path, datapath, proba=False):
+    cnv_type = ['loss', 'gain'][('gain' in model_path) * 1]
+    
+    logtransform = (model_path.split('_')[-1].split('.')[0] == 'log')
+        
+    model = open_model(model_path)
+    
+    if 'train' in datapath or 'validation' in datapath:
+        X_train, Y_train, X_val, Y_val = prepare_df(cnv_type, logtransform)
+        
+        if 'train' in datapath:
+            X, y = X_train, Y_train
+        else:
+            X, y = X_val, Y_val
+    
+    elif 'test' in datapath:
+        X, y = prepare_test(cnv_type, logtransform)
+    
+    if proba:
+        if 'xgboost' in model_path:
+            X_dmat = xgb.DMatrix(X, y)
+            yhat = model.predict(X_dmat)
+        else:
+            yhat = model.predict_proba(X)[:, 1]
+    
+    else:
+        if 'xgboost' in model_path:
+            X_dmat = xgb.DMatrix(X)
+            yhat = model.predict(X_dmat)
+            yhat = (yhat > 0.5) * 1
+        else:
+            yhat = model.predict(X)
+    
+    return yhat, y
+
